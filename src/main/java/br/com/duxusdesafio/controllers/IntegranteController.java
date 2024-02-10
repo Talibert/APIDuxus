264a7b6a257a3772abe7d4dea37355580b1fa1ce
Guilherme Taliberti <guilhermetaliberti@gmail.com>
2024-02-10 09:01:33 -0300

    cadastro de integrante implementado

--- src/main/java/br/com/duxusdesafio/controllers/IntegranteController.java
+++ src/main/java/br/com/duxusdesafio/controllers/IntegranteController.java
@@ -1,15 +1,37 @@
 package br.com.duxusdesafio.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.duxusdesafio.dtos.IntegranteDto;
 import br.com.duxusdesafio.model.Integrante;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import br.com.duxusdesafio.repositories.IntegranteRepository;
+
+
 
 @RestController
 @RequestMapping("/duxus/integrante")
 public class IntegranteController {
-    
+
+    @Autowired
+    private IntegranteRepository integranteRepository;
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<Integrante> cadastrarIntegrante(@RequestBody IntegranteDto dto ) {
+        // Instanciando um novo integrante
+        Integrante integrante = new Integrante();
+        // Atribuindo ao integrante os dados recebidos pelo dto
+        integrante.setNome(dto.getNome());
+        integrante.setFranquia(dto.getFranquia());
+        integrante.setFuncao(dto.getFuncao());
+
+        integranteRepository.save(integrante);
+
+        return ResponseEntity.ok(integrante);
+
+    }
 }
--- src/main/java/br/com/duxusdesafio/controllers/TimeController.java
+++ src/main/java/br/com/duxusdesafio/controllers/TimeController.java
@@ -1,6 +1,6 @@
 package br.com.duxusdesafio.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/duxus/time")
 public class TimeController {
     
-    @PostMapping("/helloworld")
+    @GetMapping("/helloworld")
     public String teste() {
         return "hello world";
     }
